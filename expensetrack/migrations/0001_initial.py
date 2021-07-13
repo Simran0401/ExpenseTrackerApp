5df10b4bab1bb6ad3a7ddd5c1dbb13e43106b473
Simran <simrandas0401@gmail.com>
2021-07-13 10:16:33 +0530

    [Deployment] renaming model name

--- expensetrack/migrations/0001_initial.py
+++ expensetrack/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Expense',
+            name='expensetrack_expense',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('item', models.CharField(max_length=50)),
